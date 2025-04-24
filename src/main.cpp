3b653f4e47a4b9f438abd47e8156d1e40dc9cd30
hakasapl <hakansaplakog@gmail.com>
2025-04-24 19:15:40 -0400

    0.3.3 release merging commonality with CS version - Added nullptr checks in a couple places - Checking for default set in not just first tile

--- src/main.cpp
+++ src/main.cpp
@@ -37,15 +37,20 @@ void SetupLog() {
 
 struct ExtendedSlots {
     array<RE::NiSourceTexturePtr, 6> parallax;
-    array<RE::NiSourceTexturePtr, 6> envmask;
 };
 unordered_map<uint32_t, ExtendedSlots> extendedSlots;
+RE::BGSTextureSet* defaultLandTexture;
 
 struct BSLightingShader_SetupMaterial
 {
     static void thunk(RE::BSLightingShader* shader, RE::BSLightingShaderMaterialBase const* material)
     {
         func(shader, material);
+
+		if (material == nullptr) {
+			// material is null
+			return;
+		}
 
         if (!extendedSlots.contains(material->hashKey)) {
             // hash does not exists
@@ -72,12 +77,6 @@ struct BSLightingShader_SetupMaterial
                 // set default texture
                 shadowState->SetPSTexture(heightIndex, nullptr);
             }
-
-            // ENV MASK DISABLED FOR NOW
-            //if (materialBase.envmask[textureI] != nullptr) {
-            //    const uint32_t heightIndex = EnvmaskStartIndex + textureI;
-            //    shadowState->SetPSTexture(heightIndex, materialBase.envmask[textureI]->rendererTexture);
-            //}
         }
     };
 
@@ -96,16 +95,19 @@ struct TESObjectLAND_SetupMaterial
     {
         bool vanResult = func(land);
 
-        if (land->loadedData == nullptr || land->loadedData->mesh[0] == nullptr || !vanResult) {
+        if (!vanResult || land == nullptr || land->loadedData == nullptr || land->loadedData->mesh[0] == nullptr) {
             // this is not terrain or vanilla material failed
             return vanResult;
         }
 
-        const auto& stateData = RE::BSGraphics::State::GetSingleton()->GetRuntimeData();
-
         for (uint32_t quadI = 0; quadI < 4; ++quadI) {
             // Get hash key of vanilla material
             uint32_t hashKey = 0;
+
+            if (land->loadedData->mesh[quadI] == nullptr) {
+                // continue if cannot find mesh
+                continue;
+            }
 
             const auto& children = land->loadedData->mesh[quadI]->GetChildren();
             auto geometry = children.empty() ? nullptr : static_cast<RE::BSGeometry*>(children[0].get());
@@ -126,39 +128,48 @@ struct TESObjectLAND_SetupMaterial
             }
 
             // Create array of texture sets (6 tiles)
-            std::array<RE::TESLandTexture*, 6> textureSets;
-            if (auto defTexture = land->loadedData->defQuadTextures[quadI]) {
-                textureSets[0] = defTexture;
+            std::array<RE::BGSTextureSet*, 6> textureSets;
+            auto defTexture = land->loadedData->defQuadTextures[quadI];
+            if (defTexture != nullptr && defTexture->formID != 0) {
+                textureSets[0] = defTexture->textureSet;
             }
             else {
-                textureSets[0] = GetDefaultLandTexture();
+                // this is a default texture
+                textureSets[0] = defaultLandTexture;
             }
             for (uint32_t textureI = 0; textureI < 5; ++textureI) {
-                textureSets[textureI + 1] = land->loadedData->quadTextures[quadI][textureI];
+                auto curTexture = land->loadedData->quadTextures[quadI][textureI];
+                if (curTexture == nullptr) {
+                    textureSets[textureI + 1] = nullptr;
+                    continue;
+                }
+
+                if (curTexture->formID == 0) {
+                    // this is a default texture
+                    textureSets[textureI + 1] = defaultLandTexture;
+                }
+                else {
+                    textureSets[textureI + 1] = land->loadedData->quadTextures[quadI][textureI]->textureSet;
+                }
             }
 
             // Assign textures to material
             for (uint32_t textureI = 0; textureI < 6; ++textureI) {
-                if (textureSets[textureI] == nullptr || textureSets[textureI]->textureSet == nullptr) {
+                if (textureSets[textureI] == nullptr) {
                     continue;
                 }
 
-                auto txSet = textureSets[textureI]->textureSet;
-
+                auto txSet = textureSets[textureI];
                 if (txSet->GetTexturePath(static_cast<RE::BSTextureSet::Texture>(3)) != nullptr) {
                     txSet->SetTexture(static_cast<RE::BSTextureSet::Texture>(3), extendedSlots[hashKey].parallax[textureI]);
-                    if (extendedSlots[hashKey].parallax[textureI] == stateData.defaultTextureNormalMap) {
+
+                    if (extendedSlots[hashKey].parallax[textureI] == RE::BSGraphics::State::GetSingleton()->GetRuntimeData().defaultTextureNormalMap) {
                         // this file was not found
                         if (get<1>(texturesErrorLogged.insert(txSet->GetTexturePath(static_cast<RE::BSTextureSet::Texture>(3))))) {
                             spdlog::error("Unable to find parallax map {} while setting up material", txSet->GetTexturePath(static_cast<RE::BSTextureSet::Texture>(3)));
                         }
                     }
                 }
-
-                // ENV MASK DISABLED FOR NOW
-                //if (txSet->GetTexturePath(static_cast<RE::BSTextureSet::Texture>(5)) != nullptr) {
-                //    txSet->SetTexture(static_cast<RE::BSTextureSet::Texture>(5), extendedSlots[hashKey].envmask[textureI]);
-                //}
             }
         }
 
@@ -169,19 +180,16 @@ struct TESObjectLAND_SetupMaterial
 };
 
 void onDataLoaded() {
-    // Replace default texture set if needed
+    // Get the default landscape texture set for terrain helper
     const auto defaultLandTextureSet = RE::TESForm::LookupByEditorID<RE::BGSTextureSet>("LandscapeDefault");
     if (defaultLandTextureSet != nullptr) {
-        if (auto* defaultLandTexture = GetDefaultLandTexture()) {
-            spdlog::info("Replacing default land texture set record with {}", clib_util::editorID::get_editorID(defaultLandTextureSet));
-            defaultLandTexture->textureSet = defaultLandTextureSet;
-        }
-        else {
-            spdlog::error("Default land texture set record not found");
-        }
+        spdlog::info("LandscapeDefault EDID texture set found");
+        defaultLandTexture = defaultLandTextureSet;
     }
     else {
         spdlog::info("LandscapeDefault EDID texture set not found, using default");
+        const auto bgsDefaultLandTex = *REL::Relocation<RE::TESLandTexture**>(RELOCATION_ID(514783, 400936));
+        defaultLandTexture = bgsDefaultLandTex->textureSet;
     }
 }
 
@@ -205,7 +213,7 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* sks
     }
 
     // Allocate Trampoline
-    SKSE::AllocTrampoline(64);
+    //SKSE::AllocTrampoline(64);
 
     // Hooks
     stl::write_vfunc<0x4, BSLightingShader_SetupMaterial>(RE::VTABLE_BSLightingShader[0]);
